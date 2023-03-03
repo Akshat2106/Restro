be0bb46afdefe043dc16e528647964f41a4e7f66
Akshat2106 <akshatpatidar2106@gmail.com>
2023-03-03 12:05:39 +0530

    task 1:Building the header of your restaurant app

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HeaderCartButton from "./HeaderCartButton";
+import headerImage from '../assets/React_Header.png';
+import classes from './Header.module.css';
+const Header=(props)=>{
+return (
+<React.Fragment>
+<header className={classes.header}>
+    <h1>ReactMeals</h1>
+    <HeaderCartButton/>
+</header>
+<div className={classes['main-image']}>
+    <img src={headerImage} alt='A table full of delicious food!'></img>
+</div>
+</React.Fragment>
+);
+};
+
+export default Header;
+
