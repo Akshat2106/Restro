467b2ebeb5d984140ff62e722c99abdf9d6443ce
Akshat2106 <akshatpatidar2106@gmail.com>
2023-03-04 16:33:35 +0530

    task 2 Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Header";
+import Summary from "./components/Layout/Summary";
 function App() {
   return (
     
       <Fragment>
         <Header />
+        <main>
+          <Summary/>
+        </main>
       </Fragment>
   
   );
